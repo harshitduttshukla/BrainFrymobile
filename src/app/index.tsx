fcae84f1091cafe4dc0be4a32879403c9aebf1ce
Harshit Shukla <harshitshukla6388@gmail.com>
2026-05-23 18:01:17 +0530

    second commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>HArashit </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>harshit Shukla</Text>
     </View>
   );
 }
-
-
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -20,5 +15,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 16,
+    color: '#000',
   },
 });
