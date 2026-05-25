57c9dec3e985e989a4a6e88aad3e8b91f6727bb8
Harshit Shukla <harshitshukla6388@gmail.com>
2026-05-25 12:05:43 +0530

    fix the page ui

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,12 +2,5 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
-  return (
-    <>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-      </Stack>
-      <StatusBar style="auto" />
-    </>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
