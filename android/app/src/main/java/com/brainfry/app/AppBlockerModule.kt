ab2ef40b17980496e1ce06113b75e911f69eceb3
Harshit Shukla <harshitshukla6388@gmail.com>
2026-05-30 12:15:26 +0530

    Update app icon to cartoon theme and rename package to com.brainfry.app

---
+++ android/app/src/main/java/com/brainfry/app/AppBlockerModule.kt
@@ -0,0 +1,31 @@
+package com.brainfry.app
+
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class AppBlockerModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "AppBlocker"
+    }
+
+    @ReactMethod
+    fun setShortsReelsBlocked(blocked: Boolean) {
+        val sharedPref = reactApplicationContext.getSharedPreferences("BrainFryPrefs", Context.MODE_PRIVATE)
+        with (sharedPref.edit()) {
+            putBoolean("shortsReelsBlocked", blocked)
+            commit()
+        }
+    }
+
+    @ReactMethod
+    fun openAccessibilitySettings() {
+        val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        reactApplicationContext.startActivity(intent)
+    }
+}
